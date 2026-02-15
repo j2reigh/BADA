37045b81609d553c63d932ba11df389d70abea4f
Jeanne <ljyoao@gmail.com>
2026-02-16 07:38:44 +0800

    fix(deploy): remove obsolete server/vercel.ts causing build error

--- server/vercel.ts
+++
@@ -1,9 +0,0 @@
-import { app, initPromise } from "./app";
-
-let initialized = false;
-const init = initPromise.then(() => { initialized = true; });
-
-export default async function handler(req: any, res: any) {
-  if (!initialized) await init;
-  app(req, res);
-}
